1e9f6e10ce97369d6fd4c246ed054396e0034e2e
zcracker <zcracker@163.com>
2021-07-13 16:38:47 +0800

    Update platform.sh

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -52,7 +52,7 @@ platform_do_upgrade() {
 	avm,fritzrepeater-1200 |\
 	avm,fritzrepeater-3000 |\
 	p2w,r619ac |\
-	p2w,r619ac-128m)
+	p2w,r619ac-128m |\
 	qxwlan,e2600ac-c2)
 		nand_do_upgrade "$1"
 		;;
